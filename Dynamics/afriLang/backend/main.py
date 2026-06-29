bba5241b87fc73e6ef37ca7aa0335bd84039ca8b
MynamesJc <jeanchristelsinzogan@gmail.com>
2026-06-29 08:11:41 +0100

    backend fon

--- Dynamics/afriLang/backend/main.py
+++ Dynamics/afriLang/backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 from fonspeacker import parler
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -17,4 +19,5 @@ def speak():
     return send_file(mp3, mimetype="audio/mpeg")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, debug=True)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=False)
